f3b16de6c390914e8dbd8f5393807e6fd7e73d9a
Jhonny <lucerojhonatan@gmail.com>
2021-05-13 23:02:22 -0500

    Verificar Stock
    Añadir para verificar stock a traves de un boton

--- src/ejemplo/Principal.java
+++ src/ejemplo/Principal.java
@@ -43,6 +43,7 @@ public class Principal extends javax.swing.JFrame {
         jMenu3 = new javax.swing.JMenu();
         ListaProd = new javax.swing.JMenuItem();
         jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem1 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -121,6 +122,14 @@ public class Principal extends javax.swing.JFrame {
         });
         jMenu3.add(jMenuItem4);
 
+        jMenuItem1.setText("Stock");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu3.add(jMenuItem1);
+
         BarraMenu.add(jMenu3);
 
         setJMenuBar(BarraMenu);
@@ -194,6 +203,11 @@ public class Principal extends javax.swing.JFrame {
         
     }//GEN-LAST:event_AgregarproductoMouseClicked
 
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        // TODO add your handling code here:
+        VerificarStock abrirVentana = new VerificarStock(BarraMenu);
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -240,6 +254,7 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem4;
     private javax.swing.JMenuItem jMenuItem6;
     // End of variables declaration//GEN-END:variables
